443be912d92681cc8dde0b97c45c765a5383a830
Kat <kathybuggs@gmail.com>
2017-11-02 19:23:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(name){
+  return kittens.push(name)
+}
+
+function destructivelyPrependKitten(name){
+  return kittens.unshift(name)
+}
+
+function destructivelyRemoveLastKitten(){
+  return kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten(){
+  return kittens.shift()
+}
+
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function appendKitten(name){
+  var kittens = [kittens, ...name]
+  return kittens
+}
