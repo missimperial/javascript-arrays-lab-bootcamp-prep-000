baf5e0f737976dd040357236b686b32e9d6cd441
Kat <kathybuggs@gmail.com>
2017-11-02 19:34:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,6 +21,6 @@ function destructivelyRemoveFirstKitten(){
 var kittens = ["Milo", "Otis", "Garfield"]
 
 function appendKitten(name){
-  var kitten = [kittens, ...name]
+  var kitten = kittens.push(name)
   return kitten
 }
